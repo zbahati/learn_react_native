d144159b8e6f6140eff92569ce70643059691403
zbahati <bahatizirimwabagabo@gmail.com>
2024-08-01 17:45:01 +0200

    Add file setup and file management as nextjs file system

---
+++ app/index.jsx
@@ -0,0 +1,13 @@
+import { Link } from 'expo-router';
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View className="flex-1 items-center justify-center bg-slate-300">
+      <Text className="text-3xl font-black" >Aura</Text>
+      <StatusBar style="auto" />
+      <Link href='/profile' className='text-blue-500'>Go to the profile</Link>
+    </View>
+  );
+}
