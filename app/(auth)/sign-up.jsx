c96b74f2269b32d262a21544c1a13c119531f90c
zbahati <bahatizirimwabagabo@gmail.com>
2024-08-03 09:36:29 +0200

    Finilizing signin with appwrite

--- app/(auth)/sign-up.jsx
+++ app/(auth)/sign-up.jsx
@@ -1,80 +1,74 @@
-import { View, Text, ScrollView, Image, Alert } from 'react-native'
-import React, { useState } from 'react'
+import { View, Text, ScrollView, Image, Alert } from 'react-native';
+import React, { useState } from 'react';
 import { SafeAreaView } from 'react-native-safe-area-context';
-import { images } from '../../constants'
+import { images } from '../../constants';
 import FormField from '../../components/formField';
 import CustomButton from '../../components/customButton';
-import { Link, router } from 'expo-router';
+import { Link, useRouter } from 'expo-router';
 import { CreateUser } from '../../lib/appwrite';
 
 const SignUp = () => {
-  const[isSubmitting, setIsSubmitting] = useState(false)
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const [form, setForm] = useState({ username: '', email: '', password: '' });
+  const router = useRouter();
 
-  const [form, setForm] = useState({
-    username: '',
-    email: '',
-    password: ''
-  })
-
-  const submit = async() => {
-    if(!form.username || !form.email || !form.password){
-      Alert.alert("Error", "Please fill in all the fields")
+  const submit = async () => {
+    if (!form.username || !form.email || !form.password) {
+      Alert.alert("Error", "Please fill in all the fields");
+      return;
     }
-    setIsSubmitting(true)
+    setIsSubmitting(true);
     try {
-      const result = await CreateUser(form.email, form.password, form.username)
-
-      router.replace('/home')
+      const result = await CreateUser(form.email, form.password, form.username);
+      router.replace('/home');
     } catch (error) {
-      throw new Error(error)
+      Alert.alert("Error", error.message);
+    } finally {
+      setIsSubmitting(false);
     }
   }
 
   return (
-    <SafeAreaView className=" bg-slate-950 h-full w-full">
+    <SafeAreaView className="bg-slate-950 h-full w-full">
       <ScrollView>
         <View className="min-h-[90vh] justify-center px-4">
           <Image
             source={images.logo}
             resizeMode='contain'
-            className=" h-[35px] w-[112px]"
+            className="h-[35px] w-[112px]"
           />
-
           <Text className="text-white text-2xl font-poppins-semibold mt-8">Sign up to Aora</Text>
           <FormField
-           title="username"
-           value={form.username}
-           handleChange ={(e) => setForm({...form, username: e})}
-           otherStyles ="mt-10"
+            title="username"
+            value={form.username}
+            handleChange={(e) => setForm({ ...form, username: e })}
+            otherStyles="mt-10"
           />
           <FormField
-           title="Email"
-           value={form.email}
-           handleChange ={(e) => setForm({...form, email: e})}
-           otherStyles ="mt-5"
+            title="Email"
+            value={form.email}
+            handleChange={(e) => setForm({ ...form, email: e })}
+            otherStyles="mt-5"
           />
           <FormField
-           title="Password"
-           valube={form.password}
-           handleChange ={(e) => setForm({...form, password: e})}
-           otherStyles ="mt-7"
+            title="Password"
+            value={form.password}
+            handleChange={(e) => setForm({ ...form, password: e })}
+            otherStyles="mt-7"
           />
-
           <CustomButton
-          handlePress={submit}
-          title="Sign in"
-          containerStyles="mt-7 mb-6"
+            handlePress={submit}
+            title="Sign up"
+            containerStyles="mt-7 mb-6"
           />
-          
-          <View className=" mt-5 flex-row justify-center gap-2">
-            <Text className="text-white text-lg font-poppins-light ">Have an account already?</Text>
-            <Link href="/sign-in" className='text-lg font-poppins-bold text-yellow-400'>Sign In</Link>
+          <View className="mt-5 flex-row justify-center gap-2">
+            <Text className="text-white text-lg font-poppins-light">Have an account already?</Text>
+            <Link href="/sign-in" className="text-lg font-poppins-bold text-yellow-400">Sign In</Link>
           </View>
-
         </View>
       </ScrollView>
     </SafeAreaView>
   )
 }
 
-export default SignUp
+export default SignUp;
